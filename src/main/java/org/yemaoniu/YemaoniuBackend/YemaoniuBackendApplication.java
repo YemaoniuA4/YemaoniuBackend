501994de6a86a80d63f2ccfa4a93422c7c2b29d6
DomingoWong <yhuang158@dons.usfca.edu>
2019-11-11 12:04:26 -0800

    Initial project

---
+++ src/main/java/org/yemaoniu/YemaoniuBackend/YemaoniuBackendApplication.java
@@ -0,0 +1,13 @@
+package org.yemaoniu.YemaoniuBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YemaoniuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YemaoniuBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/yemaoniu/YemaoniuBackend/YemaoniuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yemaoniu.YemaoniuBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YemaoniuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
